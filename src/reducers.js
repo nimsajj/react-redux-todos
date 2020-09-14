774a92a85bf800ee7e65609b3b586535f9c891e8
nimsajj <nimsaj.dev@gmail.com>
2020-09-14 15:09:04 +0200

    Updating the Root Reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,12 +1,7 @@
 import { combineReducers } from "redux";
+import todosReducer from "features/todos/todosSlice";
 
-import {
-  ADD_TODO,
-  REMOVE_TODO,
-  TOGGLE_TODO,
-  SET_VISIBILITY_FILTER,
-  VisibilityFilters,
-} from "./actions";
+import { SET_VISIBILITY_FILTER, VisibilityFilters } from "./actions";
 
 const { SHOW_ALL } = VisibilityFilters;
 
@@ -19,22 +14,7 @@ function filterReducer(state = SHOW_ALL, action) {
   }
 }
 
-function todoReducer(state = [], action) {
-  switch (action.type) {
-    case ADD_TODO:
-      return [...state, { id: action.id, text: action.text, completed: false }];
-    case TOGGLE_TODO:
-      return state.map((todo) =>
-        todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
-      );
-    case REMOVE_TODO:
-      return state.filter((todo) => todo.id !== action.id);
-    default:
-      return state;
-  }
-}
-
 export default combineReducers({
   visibilityFilter: filterReducer,
-  todos: todoReducer,
+  todos: todosReducer,
 });
