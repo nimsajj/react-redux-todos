b07062ad58d2038624bba700ba0f22d323d29c71
nimsajj <nimsaj.dev@gmail.com>
2020-09-15 00:22:51 +0200

    Optimizing Todo Filtering

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,25 +1,33 @@
 import { connect } from "react-redux";
-import { VisibilityFilters } from "../actions";
+import { createSelector } from "@reduxjs/toolkit";
 import { toggleTodo, removeTodo } from "../features/todos/todosSlice";
+import { VisibilityFilters } from "../features/filters/filtersSlice";
+
 import TodoList from "../components/TodoList";
 
 const { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } = VisibilityFilters;
 
-const getVisiblityFilter = (todos, filter) => {
-  switch (filter) {
-    case SHOW_ALL:
-      return todos;
-    case SHOW_COMPLETED:
-      return todos.filter((t) => t.completed);
-    case SHOW_ACTIVE:
-      return todos.filter((t) => !t.completed);
-    default:
-      return todos;
-  }
-};
+const selectTodos = (state) => state.todos;
+const selectFilter = (state) => state.visibilityFilter;
 
-const mapStateToProps = ({ todos, visibilityFilter }) => ({
-  todos: getVisiblityFilter(todos, visibilityFilter),
+const selectVisibleTodos = createSelector(
+  [selectTodos, selectFilter],
+  (todos, filter) => {
+    switch (filter) {
+      case SHOW_ALL:
+        return todos;
+      case SHOW_COMPLETED:
+        return todos.filter((t) => t.completed);
+      case SHOW_ACTIVE:
+        return todos.filter((t) => !t.completed);
+      default:
+        throw new Error("Unknown filter: " + filter);
+    }
+  }
+);
+
+const mapStateToProps = (state) => ({
+  todos: selectVisibleTodos(state),
 });
 
 const mapDispatchToProps = { toggleTodo, removeTodo };
